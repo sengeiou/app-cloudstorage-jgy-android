493bc388f75091ec764d180ebb16a6a21ccbf22f
PC-201708061841\Administrator <kejun@chinayie.com>
2017-11-24 15:50:56 +0800

    项目的第一次提交

---
+++ app/src/main/java/www/guoyie/com/delivery/easy/MainActivity.java
@@ -0,0 +1,13 @@
+package www.guoyie.com.delivery.easy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
