8e8eab30b7a23fc7487a2aaf5166063be13ac719
jiangtao <jiangtao@chinayie.com>
2017-12-05 14:46:17 +0800

    commit

--- app/src/main/java/com/guoyie/www/delivery/easy/activity/GuideActivity.java
+++ app/src/main/java/com/guoyie/www/delivery/easy/activity/GuideActivity.java
@@ -2,6 +2,7 @@ package com.guoyie.www.delivery.easy.activity;
 
 
 import android.databinding.DataBindingUtil;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 
 import com.guoyie.www.delivery.easy.R;
@@ -32,12 +33,39 @@ public class GuideActivity extends BaseActivity {
     public void initView() {
         mBinding = DataBindingUtil.setContentView(this,getLayoutId());
 
-        ArrayList<View> views = new ArrayList<>();
+        final ArrayList<View> views = new ArrayList<>();
 
         views.add(BGABannerUtil.getItemImageView(this,R.mipmap.guide_1));
         views.add(BGABannerUtil.getItemImageView(this,R.mipmap.guide_2));
         views.add(BGABannerUtil.getItemImageView(this,R.mipmap.guide_3));
 
         mBinding.bgaGuide.setData(views);
+        mBinding.bgaGuide.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position==views.size()-1){
+                    mBinding.btGotologin.setVisibility(View.VISIBLE);
+                }else {
+                    mBinding.btGotologin.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+    }
+
+    //跳转到登录页面
+    public void goToLogin(View view) {
+        startAct(MainActivity.class);
     }
 }
