293a3c5b36ec87956074f17c7c727c17d8fbad51
jiangtao <jiangtao@chinayie.com>
2017-12-05 12:36:00 +0800

    commit

---
+++ app/src/main/java/com/guoyie/www/delivery/easy/activity/LoadingActivity.java
@@ -0,0 +1,43 @@
+package com.guoyie.www.delivery.easy.activity;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.guoyie.www.delivery.easy.R;
+import com.guoyie.www.delivery.easy.base.BaseActivity;
+import com.guoyie.www.delivery.easy.databinding.ActivityLoadingBinding;
+
+public class LoadingActivity extends BaseActivity {
+    private ActivityLoadingBinding mBinding;
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_loading);
+//    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_loading;
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void initView() {
+        mBinding = DataBindingUtil.setContentView(this,getLayoutId());
+
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+        overridePendingTransition(R.anim.enter,R.anim.exit);
+    }
+
+}
