e40aac0f64e02f5ef3656de288c1b1ded0c35125
kejun@chinayie.com <kejun@chinayie.com>
2017-12-07 17:20:58 +0800

    commit

---
+++ app/src/main/java/com/guoyie/www/delivery/easy/activity/InterDetailActivity.java
@@ -0,0 +1,28 @@
+package com.guoyie.www.delivery.easy.activity;
+
+import com.guoyie.www.delivery.easy.R;
+import com.guoyie.www.delivery.easy.base.BaseActivity;
+
+/**
+ * author：柯军
+ * project：app-cloudstorage-jgy-android
+ * package：com.guoyie.www.delivery.easy.activity
+ * email：774169396@qq.com
+ * data：2017/12/7
+ */
+public class InterDetailActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_interdetail;
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
--- app/src/main/java/com/guoyie/www/delivery/easy/activity/InterOrderActivity.java
+++ app/src/main/java/com/guoyie/www/delivery/easy/activity/InterOrderActivity.java
@@ -1,6 +1,7 @@
 package com.guoyie.www.delivery.easy.activity;
 
 import android.databinding.DataBindingUtil;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,11 +9,14 @@ import android.widget.TextView;
 import com.flyco.tablayout.listener.CustomTabEntity;
 import com.flyco.tablayout.listener.OnTabSelectListener;
 import com.guoyie.www.delivery.easy.R;
+import com.guoyie.www.delivery.easy.adapter.InterListAdapter;
 import com.guoyie.www.delivery.easy.base.BaseActivity;
 import com.guoyie.www.delivery.easy.databinding.ActivityInterorderBinding;
 import com.guoyie.www.delivery.easy.entity.TabEntity;
+import com.guoyie.www.delivery.easy.widget.recyclerview.NRecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * author：柯军
@@ -21,13 +25,25 @@ import java.util.ArrayList;
  * email：774169396@qq.com
  * data：2017/12/6
  */
-public class InterOrderActivity extends BaseActivity implements View.OnClickListener {
+public class InterOrderActivity extends BaseActivity implements View.OnClickListener, InterListAdapter.OnItemClickListener, SwipeRefreshLayout.OnRefreshListener, NRecyclerView.OnLoadMoreListener {
     private ImageView                 mLeft_back;
     private TextView                  mTv_title;
     private ActivityInterorderBinding binding;
     private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
     private String                     tabTitles[]  = new String[]{"全部", "待审核(3)","未通过","入库中(3)","已完成"};
     private ImageView mIv_search;
+    //处理五情况来记录的type，和每页的情况判断情况
+
+    private List<String>  list1;//网络获取数据保存,全部的数据
+    private List<String>  list2;//网络获取数据保存,待审核的数据
+    private List<String>  list3;//网络获取数据保存,待审核的数据
+    private List<String>  list4;//网络获取数据保存,待审核的数据
+    private List<String>  list5;//网络获取数据保存,待审核的数据
+
+    private int type = 1;//1是未读，2是已读
+
+    private int page1 = 1, page2 = 1,page3 = 1,page4 = 1,page5= 1;//全部, 待审核(3),未通过,入库中(3),"已完成
+    private InterListAdapter adapter;
 
     @Override
     public int getLayoutId() {
@@ -51,8 +67,35 @@ public class InterOrderActivity extends BaseActivity implements View.OnClickList
         mIv_search.setVisibility(View.VISIBLE);
         mIv_search.setOnClickListener(this);
         initTab();
+        initRecycleView();
+
     }
 
+    private void initRecycleView() {
+        list1 = new ArrayList<>();
+        list2 = new ArrayList<>();
+        list3 = new ArrayList<>();
+        list4 = new ArrayList<>();
+        list5 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list1.add(i+"");
+            list2.add(i+"");
+            list3.add(i+"");
+            list4.add(i+"");
+            list5.add(i+"");
+        }
+
+        adapter = new InterListAdapter(mContext);
+        adapter.setData(list1);
+        adapter.setOnItemClickListener(this);
+        binding.swipeRefresh.setOnRefreshListener(this);
+        binding.nrecycler.setOnLoadMoreListener(this);
+        binding.nrecycler.setLoadMoreEnable(true);
+        binding.nrecycler.setAdapter(adapter);
+        binding.nrecycler.setErrorMessage("暂无消息提醒");
+
+
+    }
 
 
     private void initTab() {
@@ -65,6 +108,50 @@ public class InterOrderActivity extends BaseActivity implements View.OnClickList
         binding.tabLayout.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelect(int position) {
+                type=position+1;
+
+                adapter.removeLoadMoreView();
+
+                switch (type){
+                    case 1:
+                        if (list1.size()==0){
+                            loadData(type,page1);
+                        }else {
+                             adapter.setData(list1);
+                        }
+                        break;
+                    case 2:
+                        if (list2.size()==0){
+                            loadData(type,page2);
+                        }else {
+                            adapter.setData(list2);
+                        }
+                        break;
+                    case 3:
+                        if (list3.size()==0){
+                            loadData(type,page3);
+                        }else {
+                            adapter.setData(list3);
+                        }
+                        break;
+                    case 4:
+                        if (list4.size()==0){
+                            loadData(type,page4);
+                        }else {
+                            adapter.setData(list4);
+                        }
+                        break;
+                    case 5:
+                        if (list5.size()==0){
+                            loadData(type,page5);
+                        }else {
+                            adapter.setData(list5);
+                        }
+                        break;
+
+
+            }
+
 
             }
 
@@ -73,6 +160,10 @@ public class InterOrderActivity extends BaseActivity implements View.OnClickList
 
             }
         });
+    }
+
+    private void loadData(int type, int page1) {
+
     }
 
     @Override
@@ -85,5 +176,22 @@ public class InterOrderActivity extends BaseActivity implements View.OnClickList
                 break;
         }
 
+    }
+
+    @Override
+    public void onItemClick(View itemView, int position) {
+
+        startAct(InterDetailActivity.class);
+
+    }
+
+    @Override
+    public void onRefresh() {
+       binding.swipeRefresh.setRefreshing(false);
+    }
+
+    @Override
+    public void onLoadMore() {
+     binding.nrecycler.stopLoadMore();
     }
 }
