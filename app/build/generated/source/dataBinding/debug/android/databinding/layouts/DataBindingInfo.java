95989036265aaec70cfb37f52e89f4f2fe31d973
kejun@chinayie.com <kejun@chinayie.com>
2018-01-10 15:15:41 +0800

    apk优化！

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="1955615e-5b7c-40ce-928b-2f10dff01a0f")
-public class DataBindingInfo {}
--- app/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="80b0ca5d-7d91-4b36-88b8-0a0249b6a508")
-public class DataBindingInfo {}
